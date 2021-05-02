1f67c18c0641a48d1b0d8798e98cdf72a3444dac
glendo <gleneggers64@gmail.com>
2021-05-02 11:57:08 -0500

    edit get methods to include votes; add upvote put method

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,9 +1,19 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection.js');
 
 router.get('/', (req, res) => {
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
+            [
+                sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                'vote_count'
+            ]
+        ],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -24,7 +34,16 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
+            [
+                sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                'vote_count'
+            ]
+        ],
         include: [
             {
                 model: User,
@@ -55,6 +74,15 @@ router.post('/', (req, res) => {
         .catch(err => {
             console.log(err);;
             res.status(500).json(err);
+        });
+});
+
+router.put('/upvote', (req, res) => {
+    Post.upvote(req.body, { Vote })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
         });
 });
 

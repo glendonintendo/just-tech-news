ce087f087ed68d705093b20e29db5b11d7cfc9f1
glendo <gleneggers64@gmail.com>
2021-05-02 08:59:44 -0500

    add relationships between User and Post

--- models/index.js
+++ models/index.js
@@ -1,4 +1,12 @@
 const User = require('./User.js');
 const Post = require('./Post.js');
 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Post };
